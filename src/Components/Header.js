d7625d69705e1237a6b6b658b44367a1691eeba2
skotivanwalker <skotivanwalker@gmail.com>
2020-10-28 10:42:56 -0700

    Core file structure complete.

---
+++ src/Components/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+
+	render() {
+		return (
+			<div>
+				<img src={"assets/hero.jpg"} alt="golden gate bridge" className="hero"/>
+			</div>
+		);
+	}
+}
+
+export default Header;
